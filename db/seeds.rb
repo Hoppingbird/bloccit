37f7b035eb6cb2266b647ccf0d76c7110a7f9645
Hoppingbird <hoppingbird@gmail.com>
2015-05-01 18:20:25 -0400

    assignment-26-seeddata

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,8 @@
-require 'faker'
- 
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+unique_post_create = [
+  { title: "Unique Title ", body: "Unique sentence " },
+  { title: "Another Unique Title", body: "another unique sentence" },
+]
+
+unique_post_create.each do |attributes|
+  Post.where(attributes).first_or_create
+end
