c1065678c527a5fd13ae6c46f5266da5a91bb100
Hoppingbird <hoppingbird@gmail.com>
2015-05-07 19:15:05 -0400

    assignment-33-authorization

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,6 +4,20 @@
     true
    end
    
+ class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      user.present? && (record.user == user || user.admin? || user.moderator?)
+    end
+  
+end
+   
  end
  
  
