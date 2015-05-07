2ae64aab34d3a3744e1c3dfa81bb96c6ba8b70d9
Hoppingbird <hoppingbird@gmail.com>
2015-05-06 20:02:54 -0400

    assignment-32-associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	belongs_to :user
 	
-	default_scope { order('create_at DESC') }
+	#default_scope { order('create_at DESC') }
+	
+	#scope :order_by_title, -> { order('title ASC') }
+	
+	scope :ordered_by_reverse_created_at, -> { order('created_at DESC') }
+	  
+
 end
