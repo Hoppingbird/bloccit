a362652895ac595782429671407b5d4dd5c123a0
Hoppingbird <hoppingbird@gmail.com>
2015-06-01 17:40:46 -0400

    assignment-42-install-rspec

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -5,13 +5,27 @@ describe Post do
 
     before do
       @post = Post.create(title: 'post title', body: 'post')
-      3.times { @post.vote.creative(value: 1)}
-      2.times { @post.vote.creative(value: -1)}
+      3.times { @post.vote.create(value: 1)}
+      2.times { @post.vote.create(value: -1)}
     end
 
     describe '#up_votes' do
       it "counts the number of votes with value = 1" do
         expect( @post.up_votes ).to eq(3)
+      end
+    end
+
+    describe '#down_votes' do
+      it "counts the number votes with values = -1" do
+        describe Vote do
+          describe "validations" do
+            describe "value validation" do
+              it "only allows -1 or 1 as values" do
+                # your expectations here
+              end
+            end
+          end
+        end
       end
     end
 
