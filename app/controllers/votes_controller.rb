27a12ca9fa9426865783ca9d83cb6e91fa2be30b
Hoppingbird <hoppingbird@gmail.com>
2015-06-08 15:09:50 -0400

    checkpoint-50-MockingRSpec

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,18 +11,6 @@ class VotesController < ApplicationController
     redirect_to :back
   end
 
-  # def up_vote
-  #   @post = Post.find(params [:post_id])
-  #   @vote = @post.votes.where(user_id: current_user.id).first
-  #
-  #   if @vote
-  #     @vote.update_attribute(:value, 1)
-  #   else
-  #     @vote = current_user.votes.create(value: 1, post: @post)
-  #   end
-  #
-  #   redirect_to :back
-  # end
 
   private
 
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Vote do
+
+    describe "value validation" do
+
+      include TestFactories
+
+      it "only allows -1 or 1 as values" do
+        up_vote = Vote.new(value: 1)
+        expect( up_vote.valid? ).to eq(true)
+        down_vote = Vote.new(value: -1)
+        expect( down_vote.valid? ).to eq(true)
+        invalid_v = Vote.new(value: 2)
+        expect( invalid_v.valid? ).to eq(false)
+      end
+    end
+    describe 'after_save' do
+      it "calls `Post#update_rank` after save" do
+        vote = Vote.new(value: 1, post: post)
+        expect {
+          post( :update_rank, post_id: post.id )
+        }.to change{ post.update_rank }.by 1
+        vote.save
+      end
+    end
+  end
+
+
